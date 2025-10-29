fc28b41c80bf6cbd17e4f25adfbf51868ba9f6b7
gluk <nek.art.13@gmail.com>
2025-10-30 00:12:00 +0300

    chore:(server.js): исправил названия переменных

--- Server/server.js
+++ Server/server.js
@@ -1,7 +1,7 @@
 import app from './app.js';
 
 const PORT = process.env.PORT || 4200;
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Сервер запущен на порту ${PORT}`);
     console.log(`Проверить его можно здесь: http://localhost:${PORT}/api/test`);
 });
