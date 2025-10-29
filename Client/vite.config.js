89f2eabbd38052d82ee86251791e0636078288a3
gluk <nek.art.13@gmail.com>
2025-10-30 01:02:12 +0300

    fix:(package): исправил package.json для vercel

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4200',
+        target: 'https://site-delivery-production-a039.up.railway.app',
         changeOrigin: true,
         secure: false,
       }
